1b629f8d70ce7007b6d5bf627f10a2344277e7bf
Martin Wendt <mar10@wwwendt.de>
2009-11-17 13:56:07 +0100

    Fixed console output for restarting server and

--- wsgidav/server/run_reloading_server.py
+++ wsgidav/server/run_reloading_server.py
@@ -8,8 +8,9 @@ run_reloading_server
 
 Wrapper for run_server, that restarts the server when source code is modified.
 """
+import subprocess
 import sys
-from subprocess import call
+from subprocess import call, Popen 
 
 def run():
     args = sys.argv[1:]
@@ -18,8 +19,33 @@ def run():
 
     print "run_reloading_server", args 
 
-    while 3 == call(["python", "run_server.py"] + args):
-        print "run_server returned 3: restarting..."
+    try:
+        while True:
+            p = Popen(["python", "run_server.py", "-v"] + args, 
+#                      stdin=sys.stdin, 
+#                      stdout=subprocess.PIPE, 
+#                      stderr=subprocess.PIPE, 
+    #                  preexec_fn, close_fds, shell, cwd, env, universal_newlines, startupinfo, creationflags
+                      )
+            sys.stdout = p.stdout
+            sys.stderr = p.stderr
+            p.wait()
+            sys.stdout = sys.__stdout__
+            sys.stderr = sys.__stderr__
+            
+            if p.returncode == 3:
+                print "run_server returned 3: restarting..."
+            else:
+                print "run_server returned %s: terminating." % p.returncode
+                break
+            
+    except Exception, e:
+        raise e
+#    while 3 == call(["python", "run_server.py"] + args):
+#        print "run_server returned 3: restarting..."
+    
+    
+    
     
 if __name__ == "__main__":
     run()
