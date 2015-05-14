84eaf0d36958afe5c0a7fc227f24112183d27678
Martin <moogle@wwwendt.de>
2015-05-14 10:16:03 +0200

    Version 1.2.0 stable

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ Project home: https://github.com/mar10/wsgidav/
         #Development Status :: 4 - Beta
         #Development Status :: 5 - Production/Stable
 
-      classifiers = ["Development Status :: 4 - Beta",
+      classifiers = ["Development Status :: 5 - Production/Stable",
                      "Intended Audience :: Information Technology",
                      "Intended Audience :: Developers",
                      "Intended Audience :: System Administrators",
--- wsgidav/version.py
+++ wsgidav/version.py
@@ -3,5 +3,6 @@ Current WsgiDAV version number.
 
 http://peak.telecommunity.com/DevCenter/setuptools#specifying-your-project-s-version
 http://peak.telecommunity.com/DevCenter/setuptools#tagging-and-daily-build-or-snapshot-releases
+Example "1.2.0b1" or "1.2.0"
 """
-__version__ = "1.2.0b1"
+__version__ = "1.2.0"
