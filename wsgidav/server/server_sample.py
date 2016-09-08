11bd1fa1d40185807e390f8a07ee88dd2859ccef
Martin@MBP <github@wwwendt.de>
2016-09-08 07:33:23 +0200

    Update sample server to use CherryPy

--- wsgidav/server/server_sample.py
+++ wsgidav/server/server_sample.py
@@ -11,31 +11,47 @@ from wsgidav.wsgidav_app import DEFAULT_CONFIG, WsgiDAVApp
 
 __docformat__ = "reStructuredText"
 
+def main():
+    rootpath = gettempdir()
+    provider = FilesystemProvider(rootpath)
 
-rootpath = gettempdir()
-provider = FilesystemProvider(rootpath)
+    config = DEFAULT_CONFIG.copy()
+    config.update({
+        "provider_mapping": {"/": provider},
+        "user_mapping": {},
+        "verbose": 1,
+        "enable_loggers": [],
+        "propsmanager": True,      # True: use property_manager.PropertyManager                    
+        "locksmanager": True,      # True: use lock_manager.LockManager                   
+        "domaincontroller": None,  # None: domain_controller.WsgiDAVDomainController(user_mapping)
+        })
+    app = WsgiDAVApp(config)
 
-config = DEFAULT_CONFIG.copy()
-config.update({
-    "provider_mapping": {"/": provider},
-    "user_mapping": {},
-    "verbose": 1,
-    "enable_loggers": [],
-    "propsmanager": True,      # True: use property_manager.PropertyManager                    
-    "locksmanager": True,      # True: use lock_manager.LockManager                   
-    "domaincontroller": None,  # None: domain_controller.WsgiDAVDomainController(user_mapping)
-    })
-app = WsgiDAVApp(config)
+    # For an example, use CherryPy
+    from cherrypy.wsgiserver import CherryPyWSGIServer
 
-# For an example. use paste.httpserver
-# (See http://pythonpaste.org/modules/httpserver.html for more options)
-from paste import httpserver
-httpserver.serve(app, 
-                 host="localhost", 
-                 port=8080,
-                 server_version="WsgiDAV/%s" % __version__,
-                 )
+    server = CherryPyWSGIServer(
+        bind_addr=(config["host"], config["port"]),
+        wsgi_app=app,
+        server_name="WsgiDAV/%s %s" % (__version__, CherryPyWSGIServer.version),
+        )
 
-# Or we could use default the server that is part of the WsgiDAV package:
-#from wsgidav.server import ext_wsgiutils_server
-#ext_wsgiutils_server.serve(config, app)
+    try:
+        server.start()
+    except KeyboardInterrupt:
+        print("Caught Ctrl-C, shutting down...")
+    finally:
+        server.stop()
+
+    # For an example, use paste.httpserver
+    # (See http://pythonpaste.org/modules/httpserver.html for more options)
+#    from paste import httpserver
+#    httpserver.serve(app, 
+#                     host="localhost", 
+#                     port=8080,
+#                     server_version="WsgiDAV/%s" % __version__,
+#                     )
+
+
+if __name__ == "__main__":
+    main()
