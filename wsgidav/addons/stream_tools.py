62c9f6b52073fd629b9fae3cc882e26b43987058
Martin@MBP <github@wwwendt.de>
2016-10-28 09:07:59 +0200

    Add StreamingFile

--- wsgidav/addons/stream_tools.py
+++ wsgidav/addons/stream_tools.py
@@ -108,3 +108,31 @@ class FileLikeQueue(object):
     #     return result
 
     # next = __next__  # Python 2.x
+
+
+# ============================================================================
+# StreamingFile
+# ============================================================================
+
+class StreamingFile(object):
+    """A file object wrapped around an iterator / data stream."""
+
+    def __init__(self, data_stream):
+        """Intialise the object with the data stream."""
+        self.data_stream = data_stream
+        self.buffer = ''
+
+    def read(self, size=None):
+        """Read bytes from an iterator."""
+        while size is None or len(self.buffer) < size:
+            try:
+                self.buffer += self.data_stream.next()
+            except StopIteration:
+                break
+
+        sized_chunk = self.buffer[:size]
+        if size is None:
+            self.buffer = ''
+        else:
+            self.buffer = self.buffer[size:]
+        return sized_chunk
