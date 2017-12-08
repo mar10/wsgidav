9019d38483ce56af127076860417fc64a681e13a
Dobes Vandermeer <dobesv@gmail.com>
2017-12-08 14:01:27 -0800

    Change install_litmus.sh to fetch from github
    webdav.org seems to have been shut down.

--- install_litmus.sh
+++ install_litmus.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-wget http://www.webdav.org/neon/litmus/litmus-0.13.tar.gz
-tar xzf litmus-0.13.tar.gz
-cd litmus-0.13
+git clone https://github.com/tolsen/litmus.git
+cd litmus
 ./configure --with-ssl
 sudo make install
