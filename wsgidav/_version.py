6dc632ff12de72c682e168461d7fce7b88d8254a
Martin Wendt <github@wwwendt.de>
2018-06-02 09:15:55 +0200

    Bump version

--- wsgidav/_version.py
+++ wsgidav/_version.py
@@ -1,4 +1,14 @@
 """
-Current WsgiDAV version number.
+Package version number.
+
+See https://www.python.org/dev/peps/pep-0440
+
+Examples
+    Pre-releases (alpha, beta, release candidate):
+        '3.0.0a1', '3.0.0b1', '3.0.0rc1'
+    Final release:
+        '3.0.0'
+    Developmental release (to mark 3.0.0 as 'used'. Don't publish this):
+        '3.0.0.dev1'
 """
-__version__ = "2.4.1"
+__version__ = "2.4.0.dev0"
