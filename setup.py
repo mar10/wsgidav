9a2e31599e9098f1ebab6adafe95fa6030269cd3
Martin Wendt <github@wwwendt.de>
2018-06-02 09:13:10 +0200

    Test with py 3.7 (dev)

--- setup.py
+++ setup.py
@@ -190,6 +190,7 @@ setup(
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: HTTP Servers",
         "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
