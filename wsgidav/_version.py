15206a8ce840c69c9c6517f7eb30fd62847b708c
Martin Wendt <github@wwwendt.de>
2018-06-02 08:47:29 +0200

    Prepare 3.0.0 alpha

--- wsgidav/_version.py
+++ wsgidav/_version.py
@@ -1,4 +1,14 @@
 """
 Current WsgiDAV version number.
+
+See https://www.python.org/dev/peps/pep-0440
+
+Examples
+    Pre-releases (alpha, beta, release candidate):
+        '3.0.0a1', '3.0.0b1', '3.0.0rc1'
+    Final Release:
+        '3.0.0'
+    Developmental release (to mark 3.0.0 as 'used'. Don't publish this):
+        '3.0.0.dev1'
 """
-__version__ = "3.0.0"
+__version__ = "3.0.0a1"
