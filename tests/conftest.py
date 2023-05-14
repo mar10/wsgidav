ee85e7fd518f43426a3b8b424e1c86d7a59e263c
Martin Wendt <github@wwwendt.de>
2023-05-14 07:29:28 +0200

    Fix tests on Py311

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
+# # -*- coding: utf-8 -*-
 
-import os
-import sys
+# import os
+# import sys
 
-import pkg_resources
+# import pkg_resources
 
-sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
-pkg_resources.require("wsgidav")
+# sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+# pkg_resources.require("wsgidav")
