8e830031295bd9ec2241793360c16f65e41259d0
Bastien Wermeille <bastien.wermeille@gmail.com>
2023-07-25 09:04:49 +0200

    Clean read chunked response

--- wsgidav/request_server.py
+++ wsgidav/request_server.py
@@ -667,42 +667,9 @@ class RequestServer:
         # Chunked Transfer Coding
         # http://www.servlets.com/rfcs/rfc2616-sec3.html#sec3.6.1
 
-        if "Darwin" in environ.get("HTTP_USER_AGENT", "") and environ.get(
-            "HTTP_X_EXPECTED_ENTITY_LENGTH"
-        ):
-            # Mac Finder, that does not prepend chunk-size + CRLF ,
-            # like it should to comply with the spec. It sends chunk
-            # size as integer in a HTTP header instead.
-            WORKAROUND_CHUNK_LENGTH = True
-            buf = environ.get("HTTP_X_EXPECTED_ENTITY_LENGTH", "0")
-            length = int(buf)
-        else:
-            WORKAROUND_CHUNK_LENGTH = False
-            buf = environ["wsgi.input"].readline()
+        while (buf := environ["wsgi.input"].read(block_size)) is not b"":
             environ["wsgidav.some_input_read"] = 1
-            if buf == b"":
-                length = 0
-            else:
-                length = int(buf, 16)
-
-        while length > 0:
-            buf = environ["wsgi.input"].read(block_size)
             yield buf
-            if WORKAROUND_CHUNK_LENGTH:
-                environ["wsgidav.some_input_read"] = 1
-                # Keep receiving until we read expected size or reach
-                # EOF
-                if buf == b"":
-                    length = 0
-                else:
-                    length -= len(buf)
-            else:
-                environ["wsgi.input"].readline()
-                buf = environ["wsgi.input"].readline()
-                if buf == b"":
-                    length = 0
-                else:
-                    length = int(buf, 16)
         environ["wsgidav.all_input_read"] = 1
 
     def _stream_data(self, environ, content_length, block_size):
