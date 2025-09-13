4719a09cd768e34c09b8d87f658611345a45306b
Leo <i@hardrain980.com>
2025-09-13 15:21:39 +0800

    wsgidav/fs_dav_provider.py: method `set_last_modified()` accepts raw timestamp as integer, which is the format used by `X-OC-Mtime`
    Signed-off-by: Leo <i@hardrain980.com>

--- wsgidav/fs_dav_provider.py
+++ wsgidav/fs_dav_provider.py
@@ -163,7 +163,8 @@ class FileResource(DAVNonCollection):
     def set_last_modified(self, dest_path, time_stamp, *, dry_run):
         """Set last modified time for destPath to timeStamp on epoch-format"""
         # Translate time from RFC 1123 to seconds since epoch format
-        secs = util.parse_time_string(time_stamp)
+        secs = time_stamp if isinstance(time_stamp, int) else util.parse_time_string(time_stamp)
+        assert(isinstance(secs, int))
         if not dry_run:
             os.utime(self._file_path, (secs, secs))
         return True
@@ -365,7 +366,8 @@ class FolderResource(DAVCollection):
     def set_last_modified(self, dest_path, time_stamp, *, dry_run):
         """Set last modified time for destPath to timeStamp on epoch-format"""
         # Translate time from RFC 1123 to seconds since epoch format
-        secs = util.parse_time_string(time_stamp)
+        secs = time_stamp if isinstance(time_stamp, int) else util.parse_time_string(time_stamp)
+        assert(isinstance(secs, int))
         if not dry_run:
             os.utime(self._file_path, (secs, secs))
         return True
